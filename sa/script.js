493128217c6ccfe46893f1d3f3a111d55807c986
Kaz_zoc <64361796+kazzocc@users.noreply.github.com>
2024-04-29 21:40:17 -0300

    Add files via upload

---
+++ sa/script.js
@@ -0,0 +1,7 @@
+function login() {
+    // Aqui você pode adicionar o código para autenticar o usuário
+    // Este é apenas um exemplo simples, você precisará implementar a autenticação real
+    alert("Login realizado com sucesso!");
+    document.getElementById('loginForm').reset();
+  }
+  
